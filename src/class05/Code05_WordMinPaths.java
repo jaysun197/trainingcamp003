a7528ecc3a7cc9aafb37bf3f5b89c4b0cd51e0e8
jaysun197 <815533359@qq.com>
2021-10-10 18:57:58 +0800

    第五课题解注释

--- src/class05/Code05_WordMinPaths.java
+++ src/class05/Code05_WordMinPaths.java
@@ -8,6 +8,20 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
+/**
+ * 从start字符串，从list表中走，步长为1（只能走到跟自己有一个字符不一样的字符串），走到to字符串，把各种可能的路径中最短的返回。最短的可能存在多个
+ * 这是一个宽度优先+深度优先遍历的问题
+ * 思路：
+ * 1. 在list表中给每个字符串建立自己的邻居，这个仿佛是画了一张图，每个字符串就像一个点，谁旁边是谁，一目了然，记为Map<String, List<String>>nexts
+ * 实现方法有两个
+ * 	a）来到了第i个字符串，遍历list中所有字符串O(N^2)，比对收集距离为1的字符串O(k(k为字符串的平均长度))。复杂度：O(N^2*K)
+ * 	b）将所有的字符串放入set中，来到第i个字符串a，将a转化为25*k个字符串O(N*K)，分别在set中寻找是否存在并收集O(K)。复杂度：O(N*K^2)
+ * a),b)两种方案需要看N,K的大小，视情况而定
+ * 2. 根据图nexts，从start开始宽度优先遍历，记录每个字符串到start的距离。记为Map<String, Integer> distances
+ * 宽度优先：借助queue，每次poll出来的时候，遍历它的所有子，对每个子进行计算，完了后在把每个子放入queue中，直到queue空了为止
+ * 3. 根据图nexts，从start开始深度优先遍历，每深入一层，记录深入的路径，借助distances保证深入的字符串距离start单调递增，直到深入到to字符串。收集走过的路径
+ * 深度优先：借助递归，来到了start的第一个子节点curr，看curr是否符合条件需要继续深入。重点：离开curr的时候需要还原现场
+ */
 public class Code05_WordMinPaths {
 
 	public static List<List<String>> findMinPaths(
@@ -17,10 +31,6 @@ public class Code05_WordMinPaths {
 		list.add(start);
 		HashMap<String, ArrayList<String>> nexts = getNexts(list);
 		HashMap<String, Integer> distances = getDistances(start, nexts);
-		
-		
-		
-		
 		LinkedList<String> pathList = new LinkedList<>();
 		List<List<String>> res = new ArrayList<>();
 		getShortestPaths(start, end, nexts, distances, pathList, res);
@@ -39,14 +49,18 @@ public class Code05_WordMinPaths {
 	private static ArrayList<String> getNext(String word, Set<String> dict) {
 		ArrayList<String> res = new ArrayList<String>();
 		char[] chs = word.toCharArray();
+		/*curr就是遍历的字符*/
 		for (char cur = 'a'; cur <= 'z'; cur++) {
 			for (int i = 0; i < chs.length; i++) {
+				/*枚举26*k个变换*/
+				//如果当前字符时curr，就不要变了，直接下一个
 				if (chs[i] != cur) {
 					char tmp = chs[i];
 					chs[i] = cur;
 					if (dict.contains(String.valueOf(chs))) {
 						res.add(String.valueOf(chs));
 					}
+					//比对完了后需要还原回去
 					chs[i] = tmp;
 				}
 			}
@@ -58,15 +72,18 @@ public class Code05_WordMinPaths {
 			HashMap<String, ArrayList<String>> nexts) {
 		HashMap<String, Integer> distances = new HashMap<>();
 		distances.put(start, 0);
+		//借助queue
 		Queue<String> queue = new LinkedList<String>();
 		queue.add(start);
 		HashSet<String> set = new HashSet<String>();
 		set.add(start);
 		while (!queue.isEmpty()) {
+			//poll的时候计算
 			String cur = queue.poll();
 			for (String next : nexts.get(cur)) {
 				if (!set.contains(next)) {
 					distances.put(next, distances.get(cur) + 1);
+					//把子节点加进去
 					queue.add(next);
 					set.add(next);
 				}
@@ -87,16 +104,19 @@ public class Code05_WordMinPaths {
 			HashMap<String, Integer> distances,
 			LinkedList<String> path,
 			List<List<String>> res) {
+		//来到了cur字符串，先将路径记下来。
 		path.add(cur);
 		if (to.equals(cur)) {
 			res.add(new LinkedList<String>(path));
 		} else {
 			for (String next : nexts.get(cur)) {
+				//if判断保证深入next的合法性：距离单调增1
 				if (distances.get(next) == distances.get(cur) + 1) {
 					getShortestPaths(next, to, nexts, distances, path, res);
 				}
 			}
 		}
+		//离开curr，将路径擦掉，才方便看前面还有没有合法的支路（还原现场）
 		path.pollLast();
 	}
 
